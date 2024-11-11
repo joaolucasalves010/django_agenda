91f8844c3efaab67c37a7e8d8aaa8461185ad6ee
João Lucas <ylucasslalves@gmail.com>
2024-11-11 15:58:53 -0300

    criando view do contato

--- project/contact/views.py
+++ project/contact/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from contact.models import Contact
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,14 @@ def index(request):
   }
 
   return render(request, "contact/index.html", context)
+
+def contact(request, id):
+  try:
+    contact = Contact.objects.get(id=id)
+    context= {
+    "contact": contact,
+    }
+    return render(request, "contact/contact.html", context)
+  except Exception as exc:
+    print(exc)
+    raise Http404("Contato não existe")
