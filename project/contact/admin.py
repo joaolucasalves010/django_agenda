363fe61d99b9e1933bc171e3adde2d55d142e40b
João Lucas <ylucasslalves@gmail.com>
2024-11-11 15:17:32 -0300

    alterando model admin contact

--- project/contact/admin.py
+++ project/contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-  list_display = ('id' ,'first_name', 'last_name', 'email', 'phone', )
+  list_display = ('id' ,'first_name', 'last_name', 'email', 'phone', 'show', )
   ordering = ('-id', )
   list_filter = "created_date",
   search_fields = "first_name", "last_name",
   list_per_page = 10
   list_max_show_all = 200
-  list_editable = 'phone', 'email', 
+  list_editable = 'phone', 'email', 'show', 
   list_display_links = "id",
 
 @admin.register(models.Category)
