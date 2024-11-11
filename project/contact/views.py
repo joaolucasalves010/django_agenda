d5a51a7335ee8fd5e04a3a80fbd9106b233d1b9b
João Lucas <ylucasslalves@gmail.com>
2024-11-11 15:17:12 -0300

    ordenando todos os contacts em id decrescente

--- project/contact/views.py
+++ project/contact/views.py
@@ -3,8 +3,8 @@ from contact.models import Contact
 
 # Create your views here.
 def index(request):
-
-  contacts = Contact.objects.all()
+ 
+  contacts = Contact.objects.filter(show=True).order_by("-id") # Pegando todos os contacts da tabela e ordenando contatos da tabela e ordenando com ID decrescente
   context = {
     "contacts": contacts,
   }
