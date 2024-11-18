1f3c4152e80c58d6d56e501b5e36958afaf857fc
João Lucas <ylucasslalves@gmail.com>
2024-11-18 15:20:09 -0300

    view search

--- project/contact/views.py
+++ project/contact/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from contact.models import Contact
+from django.db.models import Q
 from django.http import Http404
 
 # Create your views here.
 def index(request):
- 
-  contacts = Contact.objects.filter(show=True).order_by("-id") # Pegando todos os contacts da tabela e ordenando contatos da tabela e ordenando com ID decrescente
+  contacts = Contact.objects.filter(show=True).order_by("-id")[10:20] # Pegando todos os contacts da tabela e ordenando contatos da tabela e ordenando com ID decrescente
   context = {
     "contacts": contacts,
+  }
+  return render(request, "contact/index.html", context)
+
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+  print(search_value)
+
+  if search_value == "":
+    return redirect("contact:index")
+
+  contacts = Contact.objects.filter(show=True).filter(Q(first_name__icontains=search_value) | Q(last_name__icontains=search_value) | Q(phone__icontains=search_value) | Q(email__icontains=search_value)).order_by('-id')
+  context = {
+    "contacts": contacts,
+    "search_value": search_value,
   }
 
   return render(request, "contact/index.html", context)
@@ -15,8 +29,10 @@ def index(request):
 def contact(request, id):
   try:
     contact = Contact.objects.get(id=id)
+    site_title = f"{contact.first_name} {contact.last_name} "
     context= {
     "contact": contact,
+    "site_title": site_title,
     }
     return render(request, "contact/contact.html", context)
   except Exception as exc:
