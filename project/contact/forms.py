1f0688b3f8ff9ff597f9458bd39694c44121a893
João Lucas <ylucasslalves@gmail.com>
2024-11-25 17:19:52 -0300

    form dinâmico

---
+++ project/contact/forms.py
@@ -0,0 +1,8 @@
+# type ignore
+from django import forms
+from contact.models import Contact
+
+class ContactForm(forms.ModelForm):
+  class Meta:
+    model = Contact
+    fields = ('first_name', 'last_name', 'phone', )
