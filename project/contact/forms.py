5df0256bcc1f534d33f8e4ae289ca7eec00db085
João Lucas <ylucasslalves@gmail.com>
2024-11-26 17:07:10 -0300

    cleaned data

--- project/contact/forms.py
+++ project/contact/forms.py
@@ -6,3 +6,9 @@ class ContactForm(forms.ModelForm):
   class Meta:
     model = Contact
     fields = ('first_name', 'last_name', 'phone', )
+
+  def clean(self):
+    cleaned_data = self.cleaned_data
+    print(cleaned_data)
+    return super().clean()
+      
