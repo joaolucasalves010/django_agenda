ee72552cd9c1f2357b1e198e85978f58f1d3c78c
João Lucas <ylucasslalves@gmail.com>
2024-11-28 19:10:42 -0300

    método clean para verificar erros

--- project/contact/forms.py
+++ project/contact/forms.py
@@ -1,6 +1,7 @@
 # type ignore
 from django import forms
 from contact.models import Contact
+from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
   first_name = forms.CharField(
@@ -38,6 +39,15 @@ class ContactForm(forms.ModelForm):
 
   def clean(self):
     cleaned_data = self.cleaned_data
+    first_name = cleaned_data.get("first_name")
+    last_name = cleaned_data.get("last_name")
+    
+    if last_name == first_name:
+      print("ERROR")
+      self.add_error("first_name", ValidationError("O primeiro campo não pode ser igual ao segundo", code='invalid'))
+      self.add_error("last_name", ValidationError("O primeiro campo não pode ser igual ao segundo", code='invalid'))
+      return None
+    
     print(cleaned_data)
     return super().clean()
       
