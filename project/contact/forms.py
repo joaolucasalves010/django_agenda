2601b59de1ff19a9701ae41380c3e43b87b78700
João Lucas <ylucasslalves@gmail.com>
2024-12-07 13:57:54 -0300

    Register form

--- project/contact/forms.py
+++ project/contact/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from contact.models import Contact
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
   first_name = forms.CharField(
@@ -69,4 +70,27 @@ class ContactForm(forms.ModelForm):
     return super().clean()
 
 class RegisterForm(UserCreationForm):
-  pass
+  first_name = forms.CharField(
+      required=True,
+      min_length=3,
+  )
+  last_name = forms.CharField(
+      required=True,
+      min_length=3,
+  )
+  email = forms.EmailField()
+
+  class Meta:
+      model = User
+      fields = (
+          'first_name', 'last_name', 'email',
+          'username', 'password1', 'password2',
+      )
+  def clean_email(self):
+    email = self.cleaned_data.get('email')
+    if User.objects.filter(email=email).exists():
+        self.add_error(
+          'email',
+          ValidationError('Já existe este e-mail', code='invalid')
+        )
+    return email
